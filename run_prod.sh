d48d5b8f21bbb842faa75b269e71b5859072eede
Ilya <Bezzubik12345@i.ua>
2025-06-28 18:27:41 +0300

    Remove -d flag so second command runs after first

--- run_prod.sh
+++ run_prod.sh
@@ -1,2 +1,2 @@
-docker compose -f docker-compose.prod.yml up certbot -d --build
-docker compose -f docker-compose.prod.yml up -d --build
+docker compose -f docker-compose.prod.yml up certbot --build
+docker compose -f docker-compose.prod.yml up --build
