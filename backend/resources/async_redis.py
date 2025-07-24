ad6afb7e2f7dc85ce5968ab031081d9a6ee01d6e
Ilya <Bezzubik12345@i.ua>
2025-07-24 16:30:50 +0300

    Replace two underscores with one

--- backend/resources/async_redis.py
+++ backend/resources/async_redis.py
@@ -4,53 +4,53 @@ from ..constants.redis_cache import REDIS_TIMEOUT, REDIS_RECONNECTION_ATEMPT_TIM
 
 
 class AsyncRedis:
-    __redis = None
-    __last_connection_attempt = time.time()
-    __connection_is_active = False
+    _redis = None
+    _last_connection_attempt = time.time()
+    _connection_is_active = False
 
     @classmethod
-    async def __try_reconnect(cls) -> None:
-        cls.__connection_is_active = False
+    async def _try_reconnect(cls) -> None:
+        cls._connection_is_active = False
 
         if (
-            time.time() - cls.__last_connection_attempt
+            time.time() - cls._last_connection_attempt
             > REDIS_RECONNECTION_ATEMPT_TIMEOUT
         ):
-            cls.__last_connection_attempt = time.time()
+            cls._last_connection_attempt = time.time()
 
             try:
-                cls.__redis = aioredis.Redis(
+                cls._redis = aioredis.Redis(
                     host="redis",
                     socket_connect_timeout=REDIS_TIMEOUT,
                     socket_timeout=REDIS_TIMEOUT,
                 )
 
-                await cls.__redis.ping()
+                await cls._redis.ping()
 
-                cls.__connection_is_active = True
+                cls._connection_is_active = True
             except Exception as e:
                 print(f"Redis connection error: {e}")
 
     @classmethod
     async def safe_set(cls, key: str, value: str, ex: int) -> None:
-        if cls.__connection_is_active:
+        if cls._connection_is_active:
             try:
-                await cls.__redis.set(key, value, ex=ex)
+                await cls._redis.set(key, value, ex=ex)
             except Exception as e:
                 print(f"Error setting key in Redis: {e}")
 
-                await cls.__try_reconnect()
+                await cls._try_reconnect()
         else:
-            await cls.__try_reconnect()
+            await cls._try_reconnect()
 
     @classmethod
     async def safe_get(cls, key: str) -> str | None:
-        if cls.__connection_is_active:
+        if cls._connection_is_active:
             try:
-                return await cls.__redis.get(key)
+                return await cls._redis.get(key)
             except Exception as e:
                 print(f"Error getting key from Redis: {e}")
 
-                await cls.__try_reconnect()
+                await cls._try_reconnect()
         else:
-            await cls.__try_reconnect()
+            await cls._try_reconnect()
