c996d12896f58235b90c92ce4e857256131ca461
Ilya <Bezzubik12345@i.ua>
2025-10-21 19:19:10 +0300

    chore: fail deploy if certbot fails

--- run_prod.sh
+++ run_prod.sh
@@ -1,2 +1,2 @@
-docker compose -f docker-compose.prod.yml up certbot --build
+docker compose -f docker-compose.prod.yml up certbot --build && \
 docker compose -f docker-compose.prod.yml up --build --scale certbot=0 --detach
